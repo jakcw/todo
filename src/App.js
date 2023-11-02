26f2922fabef937354e7effebee31876d60d9594
jakcw <jackwestcott2@gmail.com>
2023-11-02 17:38:53 +1100

    checkbox now crosses out task on click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import Input from './Input';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import AddIcon from '@mui/icons-material/Add';
 import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+
 
 
 function App() {
@@ -11,18 +14,20 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [editingIndex, setEditingIndex] = useState(null);
+  const [error, setError] = useState('');
 
 
-  const addTask = (event) => {
-    event.preventDefault();
-    if (inputValue !== "") {
-      setTasks([...tasks, inputValue]);
+  const addTask = (e) => {
+    e.preventDefault();
+    if (inputValue.trim() === '') {
+      setError('Please enter a task');
+      return;
     }
-    else {
-      
-    }
+    setTasks([...tasks, { text: inputValue, completed: false}]);
+    setError('');
     setInputValue('');
-  }
+  };
+
 
   const removeTask = (index) => {
     setTasks(tasks.filter((x, i) => i !== index));
@@ -30,54 +35,74 @@ function App() {
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
+    setError('');
   }
 
   const handleEditTask = (index, value) => {
     const updatedTasks = [...tasks];
-    updatedTasks[index] = value;
+    updatedTasks[index].text = value;
     setTasks(updatedTasks);
   }
 
+  const handleCheckboxChange = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed 
+    setTasks(updatedTasks);
+  }
 
   return (
     <div className="App">
       <div class="todo-container">
         <div class="todo-add">
           <form>
-            <Input value={inputValue}
+            <TextField
+              label="Add new task"
+              value={inputValue}
               onChange={handleInputChange}
+              error={!!error}
+              size="small"
+              helperText={error}
+              fullWidth
             />
-            <AddIcon onClick={addTask} sx={{cursor: 'pointer',  
-            transition: 'transform 0.1s',
-            '&:hover': {
-              transform: 'scale(1.1)',
-            }}}/>
+            <AddIcon onClick={addTask} sx={{
+              cursor: 'pointer',
+              transition: 'transform 0.1s',
+              '&:hover': {
+                transform: 'scale(1.1)',
+              }
+            }} />
           </form>
         </div>
-        <ul className="tasks">
-          {tasks.map((task, index) => (
-            <li key={index} className="task-item">
-              <div className="task-checkbox">
-                <Checkbox
-                                  />              </div>
-              <div className="task-content">
-                {editingIndex === index ? (
-                  <input
-                    value={task}
-                    onChange={(e) => handleEditTask(index, e.target.value)}
-                    onBlur={() => setEditingIndex(null)}
-                    className="task-input"
-                  />
-                ) : (
-                  <span onClick={() => setEditingIndex(index)} className="task-text">
-                    {task}
-                  </span>
-                )}
-              </div>
-              <HighlightOffIcon className="delete-button" fontSize="small" style={{ color: '#d11a2a' }} onClick={() => removeTask(index)}></HighlightOffIcon>
-            </li>
-          ))}
-        </ul>
+        {tasks.length > 0 && (
+           <ul className="tasks">
+           {tasks.map((task, index) => (
+             <li key={index} className="task-item">
+               <div className="task-checkbox">
+                 <Checkbox 
+                 checked={task.completed}
+                 onChange={() => handleCheckboxChange(index)}/>              </div>
+               <div className="task-content">
+                 {editingIndex === index ? (
+                   <TextField
+                     variant='standard'
+                     value={task.text}
+                     onChange={(e) => handleEditTask(index, e.target.value)}
+                     onBlur={() => setEditingIndex(null)}
+                     className="task-input"
+                     style={{ width: '100%' }}
+                   />
+                 ) : (
+                   <span onClick={() => setEditingIndex(index)} className={task.completed ? 'task-text completed' : 'task-text'}>
+                     {task.text}
+                   </span>
+                 )}
+               </div>
+               <HighlightOffIcon className="delete-button" fontSize="small" style={{ color: '#d11a2a' }} onClick={() => removeTask(index)}></HighlightOffIcon>
+             </li>
+           ))}
+         </ul>
+        )}
+       
 
       </div>
     </div>
